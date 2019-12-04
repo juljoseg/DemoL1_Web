a6a80607305c6cab98052bc840c2c0c92b82380f
julio <38661081+juljoseg@users.noreply.github.com>
2019-12-04 13:52:04 -0500

    Integrate with Rest Service

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -1,17 +1,72 @@
 package com.example.demo.controller;
 
+import java.awt.PageAttributes.MediaType;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.model.Message;
 
 @Controller
 public class GreetingController {
 
-  @GetMapping("/greeting")
-  public String greeting() {
-   
+	
+  @Autowired
+  private RestTemplate restTemplate;
+	
+  @Value("${message.service.url}")
+   private String url;
+	
+  @GetMapping("/message")
+  public String messageForm(Model model) {
+    
+	model.addAttribute("message", new Message());
+    
+	
+    
     return "greeting";
   }
+
+  @PostMapping("/submitMessage")
+  public String messageSubmit(@ModelAttribute Message message) {
+	  
+	  Map<String, String> params = new HashMap<String, String>();
+	    params.put("text", message.getText());
+		
+	    restTemplate.put(url, message);
+	    
+	  
+    return "greeting";
+  }
+  
+  
+//  @GetMapping("/message")
+//  public String greeting() {
+//   
+//	 
+//	  
+//    
+//       String result =  restTemplate.getForObject(
+//				"http://localhost:8081/message?text=yo", String.class);
+//	  
+//       System.out.println("------ " + result);
+//       
+//    return "";
+//  }
 
 }
